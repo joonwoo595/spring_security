e883bf9bb4e89985dfac2c41c36e707ccefb5178
joonwoo595 <jonbeo312@gmail.com>
2025-03-20 17:01:21 +0900

    Initial commit

---
+++ src/main/java/semiprojectv2m/spring_security/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package semiprojectv2m.spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
