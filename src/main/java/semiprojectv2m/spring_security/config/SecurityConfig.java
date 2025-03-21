4bc166a3a1f24d73153d3e7dad36d2bbfc34f8fa
joonwoo595 <jonbeo312@gmail.com>
2025-03-21 12:49:40 +0900

    커스텀 폼 로그인 인증 후 myinfo 출력

--- src/main/java/semiprojectv2m/spring_security/config/SecurityConfig.java
+++ src/main/java/semiprojectv2m/spring_security/config/SecurityConfig.java
@@ -14,9 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-
 import javax.servlet.http.HttpServletResponse;
-
 
 @Configuration
 @EnableWebSecurity
@@ -24,36 +22,33 @@ import javax.servlet.http.HttpServletResponse;
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
-    
-    // SecurityFilterChain : 스프링 시큐리티에서 적용된 보안규칙들을 필터로 구현해 둔 것
+
+    // SecurityFilterChain : 스프링 시큐리티에서 적용할 보안규칙들을 필터로 구현해 둔 것
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()   // CSRF 필터 끔
-                
-                .userDetailsService(userDetailsService) // userDetailsService 설정
-                
-                .authorizeRequests()    // URL 기반 인가 설정
-                .antMatchers("/user/**").hasRole("USER")    // USER 권한 사용자만 접근 가능
+            .csrf().disable() // CSRF 필터 끔
+            .userDetailsService(userDetailsService) // userDetailsService 설정
+            .authorizeRequests() // URL 기반 인가 설정
+                .antMatchers("/user/**").hasRole("USER") // USER 권한 사용자만 접근 가능
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/logout").authenticated()    // 인증 받은 사용자만 접근 가능
-                .antMatchers("/**").permitAll()    // 인증/인가 여부와 상관없이 접근 가능
+                .antMatchers("/logout").authenticated() // 인증 받은 사용자만 접근 가능
+                .antMatchers("/**", "/member/login").permitAll() // 인증/인가 여부와 상관없이 접근 가능
                 .and()
-                .formLogin()
-                .loginProcessingUrl("/api/login") // 로그인 처리 URL
-                .usernameParameter("userid")    // 아이디 매개변수 지정
-                .passwordParameter("password")  // 비밀번호 매개변수 지정
+            .formLogin()  // form login 인증 사용
+                .loginPage("/member/login")  // 커스텀 로그인 페이지 경로
+                .usernameParameter("userid") // 아이디 매개변수 지정
+                .passwordParameter("passwd") // 비밀번호 매개변수 지정
                 .defaultSuccessUrl("/") // 로그인 성공시 리다이렉트 URL
-                .failureUrl("/login?error=true")    // 로그인 실패시 리다이렉트 URL
+                .failureUrl("/login?error=true") // 로그인 실패시 리다이렉트 URL
                 .permitAll()
                 .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")// 로그아웃 설정
-                .invalidateHttpSession(true)    //세션 무효화
-                .deleteCookies("JSESSIONID")    // JSESSIONID 쿠키 삭제
+            .logout()// 로그아웃 설정
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 URL 지정
+                .logoutSuccessUrl("/") // 로그아웃 성공후 리다이렉트될 URL
+                .invalidateHttpSession(true) // 세션 무효화
+                .deleteCookies("JSESSIONID") // JSESSIONID 쿠키 삭제
                 .permitAll();
-
 
         return http.build();
     }
@@ -62,6 +57,5 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() { // 비밀번호 암호화에 사용할 인코더
         return new BCryptPasswordEncoder();
     }
-    
 
 }
---
+++ src/main/java/semiprojectv2m/spring_security/controller/UserController.java
@@ -0,0 +1,38 @@
+package semiprojectv2m.spring_security.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/member")
+public class UserController {
+
+     @GetMapping("/login")
+     public String login() {
+         return "views/login";
+     }
+
+    @GetMapping("/myinfo")
+    public String myinfo(Authentication authentication, Model model) {
+        String returnUrl = "redirect:/member/login";
+
+        // 로그인 인증이 성공했다면
+        if(authentication != null && authentication.isAuthenticated()) {
+            // 인증 완료된 사용자 정보를 가져옴
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            model.addAttribute("user", userDetails);
+            returnUrl = "/views/myinfo";
+
+        }
+//        return returnUrl;
+        return "views/myinfo";
+    }
+}
